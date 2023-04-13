a373939808dd826ce656596f9346a4dbcba85bf9
Juanma Brignole <juanmabrignole@gmail.com>
2023-04-12 22:24:07 -0300

    Objetos y Clases en JS

--- main.js
+++ main.js
@@ -108,7 +108,6 @@
 //IF ANIDADOS
 
 // let precioProducto = Number (prompt("Ingres el precio del producto:")) ;
-<<<<<<< HEAD
 
 // if (precioProducto < 20){
 //   console.log("El precio del producto es menor a 20 pesos");
@@ -194,6 +193,7 @@
 // 	console.log(otroNumero);
 	
 // } while (parseInt(otroNumero));
+
 
 /*-------------CLASE 04-----------*/
 
@@ -354,15 +354,194 @@
 
 //EJEMPLO APLICADO: CALCULAR PRECIO
 
-const suma = (a,b) => a + b;
-const resta = (a,b) => a - b;
+// const suma = (a,b) => a + b;
+// const resta = (a,b) => a - b;
 
-const iva = z => z * 0.21;
-let precioProducto = 500;
-let descuento = 50;
+// const iva = z => z * 0.21;
+// let precioProducto = 500;
+// let descuento = 50;
 
-let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento);
-console.log(nuevoPrecio);
+// let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento);
+// console.log(nuevoPrecio);
+
+/*-------------CLASE 05-----------*/
+
+//OBJETOS
+
+let nombre = "Homero";
+let edad = 39;
+let calle = "Av. Siempreviva 742";
+
+//Las variables anteriores están relacionados entre sí, por ello lo podríamos agrupar en un objeto literal
+
+const persona1 = {
+  nombre: "Homero",
+  edad: 39,
+  calle: "Av. Siempreviva 742"
+};
+
+//INSTANCIO EL OBJETO USUARIO
+
+const usuario = {
+  nombre: "Natalia",
+  apellido: "Saravia",
+  edad: 40,
+  //definida la propiedad como string
+  "pais": "Colombia",
+  esPremium: true,
+  domicilio: {
+    ciudad: "Posadas",
+    direccion: "Centenario 320",
+    cp: 3300
+  },
+  //METODOS, LO QUE EL OBJETO SABE HACER, SUS ACCIONES
+  //FORMA LARGA DE DECLARAR EL MÉTODO/FUNCIÓN
+  sayHello: function (){
+    console.log("Hola mi apellido es " + usuario.apellido);
+  },
+  //FORMA ABREVIADA DE DECLARAR EL MÉTODO/FUNCIÓN
+  diHola(){
+    console.log("Hola mi nombre es " + usuario.nombre)
+  }
+}
+
+usuario.diHola();
+usuario.sayHello();
+
+//PARA ACCEDER A LA PROPIEDAD DEFINIDA COMO STRING UTILIZO LOS CORCHETES []
+
+console.log(usuario["pais"]);
+
+//ASIGNAR VALORES A UN OBJETO VACÍO
+
+const user = {};
+
+//ACCEDO AL OBJETO VACIO
+console.log(user);
+
+//LE ASIGNO VALORES AL OBJETO
+
+user.nombre = "Macarena";
+user.apellido = "Villalba";
+user.rol = "Developer";
+user.team = "Frontend";
+
+console.log(user);
+
+//OTRA FORMA DE ASIGNAR VALORES A LAS PROPIEDADES
+user["nombre"] = "Daniela";
+
+console.log(user);
+
+//PUEDO CREAR UNA NUEVA PROPIEDAD Y ASIGNARLE UN VALOR
+user.superior = "Delfina";
+
+console.log(user);
+
+// ¿QUE ES THIS?
+//ENLAZAMIENTO EXPLICITO
+const auto = {
+  nombre: "Fuego",
+  marca: "Renault",
+  presentacion (){
+    console.log(this.nombre);
+  },
+  
+  //NUEVO OBJETO LOGAN
+  logan: {
+    nombre: "Logan",
+    presentacion (){
+      //THIS AQUÍ ADQUIERE OTRO VALOR
+      console.log(this.nombre);
+    },
+  }
+}
+
+auto.presentacion();
+//EL THIS HACE REFERENCIA AL OBJETO DONDE SE ESTA EJECUTANDO EL MÉTODO-FUNCION PRESENTACION();
+auto.logan.presentacion();
+
+
+//METODOS PERSONALIZADOS
+//FUNCION CONSTRUCTORA
+function Persona (nombre, edad, calle){
+  this.nombre = nombre;
+  this.edad = edad;
+  this.calle = calle;
+
+  //metodo personalizado
+  this.hablar = function (){
+    console.log("Hola soy " + this.nombre);
+  }
+}
+
+const persona2 = new Persona ("Homero", 39, "Av. Siempreviva 742");
+const persona3 = new Persona ("Marge", 36, "Av. Siempreviva 742");
+
+persona2.hablar();
+persona3.hablar();
+
+//OPERADOR IN Y FOR IN
+console.log("nombre" in persona2); // devuelve true
+console.log("origen" in persona2); // devuelve false
+
+for (const propiedad in persona2){
+  console.log(persona3[propiedad]);
+}
+
+//METODOS EN OBJETOS JS
+let cadena= "HOLA CODER";
+
+console.log(cadena.length);
+console.log(cadena.toLocaleLowerCase());
+console.log(cadena.toUpperCase());
+
+//CLASES EN EMAC6 QUE REEMPLAZA A LA FUNCIÓN CONSTRUCTORA
+
+class Usuario {
+  constructor (nombre, apellido, edad){
+    this.nombre= nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+  }
+  //metodo de la clase
+  hablar(){
+    console.log("Hola soy " + this.nombre);
+  }
+}
+
+const usuario1 = new Usuario ("Magalí", "Nieva", 30);
+console.log(usuario1);
+usuario1.hablar();
+
+//EJEMPLO APLICADO
+
+class Producto {
+  constructor(nombre, precio) {
+      this.nombre  = nombre.toUpperCase();
+      this.precio  = parseFloat(precio);
+      this.vendido = false;
+  }
+
+  //metodos
+  sumaIva () {
+      console.log(this.precio = this.precio * 1.21);
+  }
+  
+  vender () {
+    console.log(this.vendido = true);
+  }
+}
+
+//CONSTRUYO LOS DIFERENTES OBJETOS PRODUCTOS A PARTIR DE LA CLASE PRODUCTO
+
+const producto1 = new Producto("arroz", "125");
+const producto2 = new Producto("fideo", "50");
+
+
+producto1.sumaIva();
+producto2.sumaIva();
+producto1.vender();
 
 
 
@@ -372,92 +551,10 @@ console.log(nuevoPrecio);
 
 
 
-=======
->>>>>>> 9e6d65ef142d71f7c45fbfc0c4b7b49952dce0ef
 
-// if (precioProducto < 20){
-//   console.log("El precio del producto es menor a 20 pesos");
-// } else if (precioProducto < 50){
-//   console.log("El precio ingresado es menor que 50 pesos");
-// } else if (precioProducto < 100){
-//   console.log("El precio ingresado es menor que 100 pesos");
-// } else {
-//   console.log("El precio ingresado es mayor a 100 pesos");
-// }
 
-// //VARIABLES BOOLEANAS
 
-// let edad = Number (prompt("Ingrese su edad"));
-// const ESMAYOR = (edad >= 18);
 
-// if (ESMAYOR) {
-//   alert("Bienvenido/a al sitio web: ");
-// } else {
-//   alert("Debes ser mayor de edad para ingresar");
-// }
-
-//OPERADORES LÓGICOS
-
-// let nombreUsuario = prompt("Ingrese su nombre: ");
-// let apellidoUsuario = prompt("Ingrese su apellido: ");
-
-// if ((nombreUsuario != "") || (apellidoUsuario != "")){
-//     alert("Bienvenido" + nombreUsuario + " " + apellidoUsuario);
-// } else {
-//   alert("ERROR: falta algún dato!");
-// } 
-    
-
-//CICLOS 
-//CICLO FOR
-
-//INCREMENTO
-// for (let i= 0; i<4;i++){
-// console.log("Iteración n°: " + i);
-// }
-
-//DECREMENTO
-// for (let i= 4; i>=1;i--){
-// 	console.log("Iteración n°: " + i);
-// 	}
-
-//TURNERA
-
-// for (let i= 1; i<=20;i++){
-// 	let ingresarNombre = prompt("Ingresar nombre: ");
-// 	alert("Turno N° "+ " " + i + " Nombre: " + ingresarNombre);
-// }
-
-//CICLO WHILE
-//PROGRAMITA DONDE EL USUARIO CARGA DATOS HASTA QUE INGRESA ESC 
-
-// let entrada = prompt("Ingrese un dato: ");
-
-// while (entrada != "ESC"){
-// 	alert("El usuario ingresó: " + entrada);
-// 	entrada = prompt("Ingrese oro dato: ");
-// }
-
-//LA PALABRA CORRECTA
-
-// let palabra = "";
-
-// while (palabra != "Hola"){
-// 	palabra = prompt("Ingrese la palabra correcta");
-// } 
-
-// alert("Ingresaste la palabra correcta!");
-
-//CICLO DO WHILE
-//PROGRAMITA QUE CORTA CUANDO EL USUARIO NO INGRESA UN NÚMERO
-
-let otroNumero = 0;
-
-do {
-	otroNumero= prompt("Ingrese un número: ");
-	console.log(otroNumero);
-	
-} while (parseInt(otroNumero));
 
 
 
